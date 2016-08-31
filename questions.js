3c943a147c50da70570226616332ae680a7e9b03
matutter <matutter4@gmail.com>
2016-08-31 16:54:03 -0400

    more questiosn

--- questions.js
+++ questions.js
@@ -68,6 +68,50 @@ module.exports.list = [
       How many mL(s) does the nurse administer?
     `
     return wrap(data, text)
+  },
+  function() {
+    var data = {
+      dose: rand(20, 180),
+      ratio: rand(100, 900),
+      answer: null,
+      unit: 'mL(s)',
+      info: 'Round to the tenths place.'
+    }
+
+    data.answer = Math.round( (data.dose / data.ratio) * 10 * 1000 ) / 10 
+
+    const text = dedent`
+      The provider orders haloperidol ${data.dose} g IM.
+      There are ${data.ratio} mg/mL of haloperidol. 
+      How many ${data.unit} does the nurse administer?
+    `
+    return wrap(data, text)
+  },
+  function() {
+    var data = {
+      dose: rand(1333, 4555) / 10,
+      solution: rand(10, 40),
+      minutes: rand(30, 60),
+      ratio: rand(100, 900) / 1000,
+      answer: null,
+      unit: 'mL/hour',
+      info: 'Round to the tenths place.'
+    }
+
+    var dose_ml = data.dose / data.ratio
+    var ratio_solution = dose_ml / data.solution
+    var total_minutes = ratio_solution * data.minutes
+    var total_hours = total_minutes / 60
+    
+    data.answer = Math.round(total_hours * 10) / 10 
+
+    const text = dedent`
+      The patient is ordered Zosyn ${data.dose}g.
+      The Zosyn is mixed in ${data.solution} mLs of solution, and is to infuse over ${data.minutes} minutes.
+      There are ${data.ratio}g of Zosyn per mL of solution (${data.ratio}g/mL).
+      At what rate (${data.unit}) does the nurse set the pump?
+    `
+    return wrap(data, text)
   }
 ]
 
